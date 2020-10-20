386ed12403fd1531d99dae52d6c20fcfad9fcd4e
ArturAllen <72237233+ArturAllen@users.noreply.github.com>
2020-10-20 14:55:31 -0400

    Add files via upload

--- wraptest/src/Main.hs
+++ wraptest/src/Main.hs
@@ -6,34 +6,55 @@ width, height :: Int
 width = 100
 height = 100
 
+fps :: Float
+fps = 25
+
 toInt :: Float -> Int
 toInt = round
 
---Desenha um pixel na tela; corrige a posicao do ponto se estiver fora da area de exibicao
-ponto :: (Int, Int) -> Picture
-ponto (x, y) = translate x' y' (rectangleSolid 1 1)
+wrap :: (Int, Int) -> (Int, Int)
+wrap (x, y) = (x', y')
   where
     x'
-     | x < (-width) = fromIntegral ( 2*width + x)
-     | x > width = fromIntegral ( (-2)*width + x)
-     | otherwise = fromIntegral x
+      | x < -width = x + 2*width
+      | x >  width = x - 2*width
+      | otherwise = x 
     y'
-     | y < (-height) = fromIntegral ( 2*height + y)
-     | y > height = fromIntegral ( (-2)*height + y)
-     | otherwise = fromIntegral y
+      | y < -width = y + 2*height
+      | y >  width = y - 2*height
+      | otherwise = y
+
+--Desenha um pixel na tela
+ponto :: (Int, Int) -> Picture
+ponto (xi, yi) = translate xf yf (rectangleSolid 1 1)
+  where
+    xf = fromIntegral x'
+    yf = fromIntegral y'
+    (x', y') = wrap (xi, yi)
+
+eqReta :: Int -> Int -> Int -> Int -> Int -> Int
+eqReta x1 x2 y1 y2 y = (x1-x2)*(y-y2)`div`(y1-y2) + x2
+
+pontosNoEixo :: Int -> Int -> Int -> Int -> [Int]
+pontosNoEixo x1 x2 y1 y2 = map eq [y1..y2]
+    where
+      eq = eqReta x1 x2 y1 y2
+
+faixa :: Int -> Int -> [Int]
+faixa a b 
+  | a > b = [b..a]
+  | otherwise = [a..b]
 
 linhaHMaior :: Int -> Int -> Int -> Int -> [(Int, Int)]
-linhaHMaior x1 x2 y1 y2 = 
-  if emOrdem then zip ab [y1..y2] else zip ab [y2..y1]
+linhaHMaior x1 x2 y1 y2 = zip ab (faixa y1 y2)
     where
-      ab = if emOrdem then [ (x1-x2)*(y-y2)`div`(y1-y2) + x2  | y <- [y1..y2] ] else [ (x1-x2)*(y-y2)`div`(y1-y2) + x2  | y <- [y2..y1] ]
+      ab = if emOrdem then pontosNoEixo x1 x2 y1 y2 else pontosNoEixo x2 x1 y2 y1
       emOrdem = y2 >= y1
 
 linhaWMaior :: Int -> Int -> Int -> Int -> [(Int, Int)]
-linhaWMaior x1 x2 y1 y2 = 
-  if emOrdem then zip [x1..x2] ord else zip [x2..x1] ord
+linhaWMaior x1 x2 y1 y2 = zip (faixa x1 x2) ord
     where
-      ord = if emOrdem then [ (y1-y2)*(x-x2)`div`(x1-x2) + y2  | x <- [x1..x2] ] else [ (y1-y2)*(x-x2)`div`(x1-x2) + y2  | x <- [x2..x1] ]
+      ord = if emOrdem then pontosNoEixo y1 y2 x1 x2 else pontosNoEixo y2 y1 x2 x1
       emOrdem = x2 >= x1
 
 -- Desenha uma linha entre dois pontos
@@ -61,35 +82,40 @@ poligono ps@(p:s) = pictures ciclo
 circulo :: (Int, Int) -> Int -> [(Int, Int)]
 circulo (x, y) r = zip ab ord
   where
-    ab = [x + calcAb x' | x' <- [0..31] ]
-    ord = [y + calcOrd y' | y' <- [0..31] ]
+    ab = map (\x' -> x + calcAb x') [0..31]
+    ord = map (\y' -> y + calcOrd y') [0..31]
     calcAb  x' = toInt ( fromIntegral r * cos( fromIntegral x' *2*pi/ 32) )
     calcOrd y' = toInt ( fromIntegral r * sin( fromIntegral y' *2*pi/ 32) )
 
 -- faz translacao de um ponto
 translada :: Int -> Int -> (Int, Int) -> (Int, Int)
-translada dsx dsy (x, y) = (x+dsx, y+dsy)
+translada dsx dsy (xi, yi) = (xf, yf)
+  where
+    xf = xi + toInt ( fromIntegral dsx / fps)
+    yf = yi + toInt ( fromIntegral dsy / fps)
 
-primEl, secEl :: (Int, Int) -> Int
-primEl (x, _) = x
-secEl  (_, y) = y
+mediaCom :: ((Int, Int) -> Int) -> [(Int, Int)] -> Int
+mediaCom f ps = sum (map f ps) `div` length ps
 
 pontoMedio :: [(Int, Int)] -> (Int, Int)
 pontoMedio ps = (x, y)
   where
-    x = sum (map primEl ps) `div` length ps
-    y = sum (map secEl ps) `div` length ps
+    x = mediaCom fst ps
+    y = mediaCom snd ps
 
--- Faz rotacao com centro na origem de um ponto
+-- Faz rotacao de um ponto com centro na origem
 rotaciona :: Int -> (Int, Int) -> (Int, Int)
 rotaciona ang (x, y) = (x', y')
   where
-    x' = toInt (fromIntegral x * cos (ang'/20) - fromIntegral y * sin (ang'/20) )
-    y' = toInt (fromIntegral x * sin (ang'/20) + fromIntegral y * cos (ang'/20) )
-    ang' = fromIntegral ang
+    x' = toInt (fromIntegral x * cos ang' - fromIntegral y * sin ang' )
+    y' = toInt (fromIntegral x * sin ang' + fromIntegral y * cos ang' )
+    ang' = fromIntegral ang / fps
 
 tempoDisc :: Float -> Int
-tempoDisc dt = toInt (20*dt)
+tempoDisc dt = toInt (fps*dt)
+
+tuplaCom :: (a -> b) -> (a, a) -> (b, b)
+tuplaCom f (x, y) = (f x, f y)
 
 animacao :: Float -> Picture
 animacao dt = pictures figuras
@@ -97,12 +123,11 @@ animacao dt = pictures figuras
     figura1 = poligono modeloAtual
     figura2 = poligono ( circulo (x, y) 30 )
     figuras = [figura1, figura2]
-    x = fromIntegral $ primEl $ pontoMedio modeloAtual
-    y = fromIntegral $ secEl $ pontoMedio modeloAtual
+    (x, y) = tuplaCom fromIntegral (pontoMedio modeloAtual)
     modeloAtual = map (translada dsx dsy . rotaciona ang) modelo
     dsy = 3 * dsx
-    dsx = tempoDisc dt
-    ang = dsx
+    dsx = 10 * tempoDisc dt
+    ang = tempoDisc dt
     modelo = [(-20,-13), (0, 27), (20, -13)]
 
 main :: IO ()
